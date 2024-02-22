53754a7f76f1ae8cad3ad63042db274576fa7b85
eacastel <emilio@aliigo.com>
2024-02-22 03:33:59 -0800

    re-index-keta

--- src/components/layout/HeaderKetamine.js
+++ src/components/layout/HeaderKetamine.js
@@ -5,30 +5,30 @@ import Button from '../Button';
 
 const Header = () => (
   <header className="sticky top-0 bg-white shadow z-20">
-    <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
+    <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-6">
       <div className="flex items-center text-2xl ">
-        <div className="w-[200px]"><a href="#top">
+        <div className=""><a href="#top">
           <StaticImage src="../../images/ppwg-logo.png" alt="Pacific Pain & Wellness Group Logo" /></a>
         </div>
       </div>
       <div className="flex mt-4 sm:mt-0 font-bold">
 
-        <AnchorLink className="px-2 md:px-4" href="#approach">
+        <AnchorLink className="px-2 md:px-6" href="#approach">
           Approach
         </AnchorLink>
-        <AnchorLink className="px-2 md:px-4" href="#process">
+        <AnchorLink className="px-2 md:px-6" href="#process">
           Process
         </AnchorLink>
-        <AnchorLink className="px-2 md:px-4" href="#testimonials">
+        <AnchorLink className="px-2 md:px-6" href="#testimonials">
           Testimonials
         </AnchorLink>
-        <AnchorLink className="px-2 md:px-4" href="#contact">
+        <AnchorLink className="px-2 md:px-6" href="#contact">
           Contact
         </AnchorLink>
 
       </div>
       <div className="hidden md:block">
-        <Button className="text-sm font-bold">Get Started</Button>
+        <Button className="text-sm font-bold">Get&nbsp;Started</Button>
       </div>
     </div>
   </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,138 +1,191 @@
 import React from 'react';
-import Button from '../components/Button';
-import CallButton from '../components/CallButton';
-import Card from '../components/Card';
-import CustomerCard from '../components/CustomerCard';
-import LabelText from '../components/LabelText';
-import Layout from '../components/layout/Layout';
-import SplitSection from '../components/SplitSection';
-import customerData from '../data/customer-data';
+import Layout2 from '../components/layout/Layout2';
 import { StaticImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby';
+import ButtonKetamine from '../components/ButtonKetamine';
 
-const Index = () => (
-  <Layout>
-    <section id="top" className="pt-20 md:pt-40">
-      <div className="container mx-auto px-8 lg:flex">
-        <div className="text-center lg:text-left lg:w-1/2 p-2">
-          <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none text-blue-800">
-            Mental Health & <br />Pain Management Solutions
-          </h1>
-          <p className="text-xl lg:text-2xl mt-6 font-light">
-          Find relief with our personalized <br />and innovative treatments
-          </p>
-          <p className="mt-8 md:mt-12">
-            <Button size="lg">Get Started</Button>
-            <CallButton phoneNumber="(310) 437-7399" size="lg">Call Us Now</CallButton>
+const Inside = () => {
+  return (
+    <Layout2>
 
-          </p>
-          <p className="mt-4 text-gray-600">Take the first step towards your new life</p>
-        </div>
-        <div className="lg:w-1/2">
-        <StaticImage src="../images/heroimage.jpg" alt="Pacific Pain & Wellness Group Logo" className="rounded-md shadow" />
-        </div>
-      </div>
-    </section>
-    <section id="services" className="py-20 lg:pb-40 lg:pt-40">
-      <div className="container mx-auto text-center">
-        <h2 className="text-3xl lg:text-5xl font-semibold text-blue-800">Our Services</h2>
-        <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
-          <div className="flex-1 px-2">
-            <Card className="mb-8">
-              <p className="font-semibold text-3xl text-blue-800">Pain Management</p>
-              <p className="font-semibold text-xl mt-4 text-gray-600">
-              Uncover a life with less pain through innovative and personalized pain management treatments.
-              </p>
-              <p className="mt-4">
-              We offer specific pain management treatments for complex regional pain syndrome (CRPS), neuropathic pain, migraines, as well as muscular, skeletal (bone), neuropathic (produced by injured nerves), post-surgical pain, and cancer pain.
-              </p>
-              <p className="mt-8 md:mt-12">
-            <Button size="lg">Get Started</Button>
-          </p>
-            </Card>
-          </div>
-          <div className="flex-1 px-2">
-            <Card className="mb-8">
-              <p className="font-semibold text-3xl text-blue-800">Mental Health</p>
-              <p className="font-semibold text-xl mt-4 text-gray-600">
-              Discover a safe path towards mental balance with therapies tailored to your individual needs.
-              </p>
-              <p className="mt-4">
-              We offer treatment for a range of mental health disorders, including anxiety, depression, and PTSD. Our licensed mental health providers have decades of collective experience providing psychiatric care and psychotherapy services to help individuals overcome these challenges.
-              </p>
-              <p className="mt-8 md:mt-12">
-            <Button size="lg">Get Started</Button>
-          </p>
-            </Card>
-          </div>
-        </div>
-      </div>
-    </section>
-    <SplitSection
-      id="about"
-      primarySlot={
-        <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-3xl font-semibold leading-tight">About Us</h3>
-          <p className="mt-8 text-xl font-light leading-relaxed">
-          Pacific Pain & Wellness Group is the premier pain management and mental health clinic in Torrance, CA, that offers a wide array of services. Our team of experts provides evidence-based personalized solutions to heal patients suffering from chronic pain and mental health conditions, utilizing both traditional and new treatments. We offer in-person and telehealth appointments to ensure accessible care for our patients.
-          </p>
-        </div>
-      }
-      secondarySlot={<StaticImage src="../images/Pacific-Pain-and-Wellness-Group.jpg" alt="Pacific Pain & Wellness Group Offices in Torrance, CA" className="rounded-md shadow" />}
-    />
-    <SplitSection
-    id="dr-ananth"
-      reverseOrder
-      primarySlot={
-        <div className="lg:pl-32 xl:pl-48">
-          <h3 className="text-3xl font-semibold leading-tight">
-            Dr. Kartik Ananth, MD, MBA
-          </h3>
-          <p className="mt-8 text-xl font-light leading-relaxed">
-          Dr. Ananth has dedicated his life to helping patients overcome challenges so they can enjoy a fuller life. His initial interest  in medicine stemmed from his father, who was also a physician. As a child he admired his father’s knowledge and passion for patient care. From an early age, Dr. Ananth dove into science to learn more about how it can be used to help people.
+      {/* First Section */}
 
+      <section className="bg-hero-pattern bg-no-repeat bg-cover md:bg-custom-large bg-custom-small overflow-hidden">
+        <div className="container mx-auto px-4 py-8 z-10 w-full">
 
-          </p>
-        </div>
-      }
-      secondarySlot={<StaticImage src="../images/dr-kartik-ananth.jpg" alt="Pacific Pain & Wellness Group Offices in Torrance, CA" className="rounded-md shadow" />}
-    />
-    <SplitSection
-      id="about"
-      primarySlot={
-        <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-3xl font-semibold leading-tight">
-          Our focus is on you - your comfort, your satisfaction, and your healing
-          </h3>
-          <p className="mt-8 text-xl font-light leading-relaxed">
-        Our services are centered around you, with convenient and quick appointments for patients to see a provider via telehealth for mental health and in-person for pain management. We understand that pain and mental health are deeply interconnected, which is why we offer a complete mind and body solution to help you achieve overall wellness. Our state-of-the-art facility and convenient parking make every visit as easy and stress-free as possible. 
-          </p>
-        </div>
-      }
-      secondarySlot={<StaticImage src="../images/tms-room.jpg" alt="Pacific Pain & Wellness Group Offices in Torrance, CA" className="rounded-md shadow" />}
-    />
-    <section id="testimonials" className="py-20 lg:py-40">
-      <div className="container mx-auto">
-        <LabelText className="mb-8 text-gray-600 text-center">What our patients are saying</LabelText>
-        <div className="flex flex-col md:flex-row md:-mx-3">
-          {customerData.map(customer => (
-            <div key={customer.customerName} className="flex-1 px-3">
-              <CustomerCard customer={customer} />
+          <div className="grid grid-cols-1 md:grid-cols-9 gap-4">
+            <div className="md:col-span-5 text-left">
+              <div className="mt-8">
+                <div className="text-lg leading-6 font-semibold uppercase tracking-wide text-gray-700 py-4">Transform Your Life</div>
+                <h1 className="mb-2 text-4xl font-poppins font-semibold tracking-tight leading-tight md:text-5xl">Safe & Effective<br />Ketamine Therapy</h1>
+                <p className="text-lg leading-8 text-gray-800 max-w-prose">
+                  Is traditional care not getting the results you’re looking for? It’s time to try something new: Holistic, evidence-based programs, including <strong>ketamine therapy</strong> and integration, to help you achieve personal and clinical breakthroughs.
+                </p>
+                <div className="my-6 border-b border-gray-200 w-full"></div>
+                <div className="text-base leading-8 text-gray-800">
+                  Affordable treatments as low as<br /><span className="font-semibold text-2xl">$125 per session</span><br />
+                  <span className=' text-sm leading-none'>Health insurance is not accepted at this time.</span>
+                </div>
+                <div className="inline-block pt-4 mt-6 py-6 ">
+                <ButtonKetamine size="lg">Am I a Candidate?</ButtonKetamine>
+                </div>
+              </div>
             </div>
-          ))}
+            <div className="hidden lg:block lg:col-span-1"></div>
+          </div>
         </div>
-      </div>
-    </section>
-    <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
-      <h3 className="text-5xl font-semibold">Ready to begin your healing journey?</h3>
-      <p className="mt-8 text-xl font-light">
-      At Pacific Pain & Wellness, we are committed to helping you achieve sustainable relief and restoring your life.
-      </p>
-      <p className="mt-8">
-        <Button size="xl">Get Started Now</Button>
-        <CallButton phoneNumber="(310) 437-7399" size="xl">Call Us Now</CallButton>
-      </p>
-    </section>
-  </Layout>
-);
+        <div className="md:hidden">
+        <StaticImage src="../images/hero-banner-mobile.png" alt="happy smiling 30 year old attractive brunette in a thick cotton sweater" className="w-full object-cover z-10 md:hidden"  />
+        </div>
+      </section>
+      <div className="bg-blue-600 w-full h-2"></div>
 
-export default Index;
+
+      {/* Second Section */}
+
+      <section id="approach" className="py-20 bg-white">
+        <div className="max-w-screen-xl mx-auto px-4 md:px-6 lg:px-8">
+          {/* Headline Centered */}
+          <div className="text-center mb-10">
+            <div className="text-xl font-normal mb-4 text-gray-600">A Holistic Approach to Mental Wellness</div>
+            <h2 className="mb-2 text-4xl font-poppins font-semibold tracking-tight leading-tight md:text-5xl">Rediscover Yourself</h2>
+          </div>
+
+          {/* Grid layout for video and text content for md and lg screens */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+            {/* Image/Video Thumbnail */}
+            <a href="#" className="block" aria-label="open lightbox">
+            <StaticImage src="../images/relax-top.jpg" alt="John's ketamine testimonial" className="mx-auto rounded-lg"  />
+            </a>
+
+            {/* Text Content */}
+            <div className="space-y-5">
+              <p className="text-xl text-bold text-gray-700">
+              Discover the transformative power of ketamine therapy for chronic pain and mental health relief at Pacific Pain & Wellness Group.
+              </p>
+              <p className="text-base text-gray-600">
+              At Pacific Pain & Wellness Group, ketamine infusions emerge as a beacon of hope for individuals grappling with chronic pain and stubborn mental health conditions. By integrating ketamine therapy into a comprehensive treatment plan, we aim to unlock new possibilities for healing and improvement, especially for those who have found little relief in traditional treatments.
+              </p>
+              <p className="text-base text-gray-600">
+              Our patient-centric approach ensures each treatment plan is as unique as the individuals we serve, promising not just symptomatic relief but a journey towards lasting wellness. Ketamine therapy, with its rapid effectiveness, stands out as a key component in our holistic strategy to combat pain, depression, anxiety, and PTSD, guiding our patients towards a brighter, more hopeful future.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Third Section */}
+
+      <section id="process" className="py-20 bg-gradient-to-r from-blue-100 to-blue-300">
+        <div className="max-w-screen-xl mx-auto px-4 md:px-6 lg:px-8">
+          <div className="text-center mb-10">
+            <div className="text-xl font-normal mb-4 text-gray-600">The Process</div>
+            <h2 className="mb-2 text-4xl font-poppins font-semibold tracking-tight leading-tight md:text-5xl">You deserve to feel better.</h2>
+            <p className="font-normal text-xl leading-relaxed text-gray-700">
+            Pioneering mental health and pain management with expertly supervised ketamine infusion therapy, ensuring personalized and effective care.
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {/* Process Step 1 */}
+            <div className="bg-white shadow-lg rounded-xl p-8">
+              <div className="text-purple-500 font-poppins text-6xl">01</div>
+              <h3 className="font-poppins font-semibold text-xl mb-4">Take the Assessment</h3>
+              <p className="font-normal text-lg leading-relaxed text-gray-700">
+              Begin your path to wellness with a brief assessment to understand if Ketamine therapy suits your needs."
+              </p>
+            </div>
+
+            {/* Process Step 2 */}
+            <div className="bg-white shadow-lg rounded-xl p-8">
+              <div className="text-purple-500 font-poppins text-6xl">02</div>
+              <h3 className="font-poppins font-semibold text-xl mb-4">Explore your options</h3>
+              <p className="font-normal text-lg leading-relaxed text-gray-700">
+                If qualified, you can <a href="/resources/ketamine-cost" className="text-blue-600 hover:text-blue-800">select a treatment path</a> or <a href="#schedule" className="text-blue-600 hover:text-blue-800">schedule a consultation</a> by filling out the consultation information form.
+              </p>
+            </div>
+
+            {/* Process Step 3 */}
+            <div className="bg-white shadow-lg rounded-xl p-8">
+              <div className="text-purple-500 font-poppins text-6xl">03</div>
+              <h3 className="font-poppins font-semibold text-xl mb-4">Start feeling better</h3>
+              <p className="font-normal text-lg leading-relaxed text-gray-700">
+              Our clinicians and doctors will guide you through an integrative approach to support your healing journey.
+              </p>
+            </div>
+          </div>
+
+          <div className="text-center mt-12">
+        
+                <ButtonKetamine size="lg">Take The Asessment</ButtonKetamine>
+                </div>
+        </div>
+      </section>
+
+
+      {/* Fourth Section */}
+
+
+      <section id="testimonials" className="py-20 bg-white">
+        <div className="max-w-screen-xl mx-auto px-4">
+          <div className="text-center mb-16">
+            <h2 className="mb-2 text-4xl font-poppins font-semibold tracking-tight leading-tight md:text-5xl">Client Success Stories</h2>
+            <p className="font-medium text-lg">Better Together</p>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {/* Testimonial 1 */}
+            <div className="space-y-4">
+            <div className="flex justify-center">
+            <StaticImage src="../images/testimonial-beth.jpg" alt="John's ketamine testimonial" className="mx-auto rounded-full lg:w-3/4" />
+            </div>
+              <p className="text-center text-gray-600">“Everyone was helpful and attentive, from the front desk staff, to the nurse and the PA. The appt ran on time. My issue was addressed and I was scheduled for my procedure the very next week. Office is clean and comfortable. I would highly recommend the services of Pacific Pain & Wellness”</p>
+              <p className="text-center font-semibold">Kery W.</p>
+              <p className="text-center text-sm text-gray-500">Pacific Pain & Wellness Client</p>
+            </div>
+            {/* Testimonial 2 */}
+            <div className="space-y-4">
+            <div className="flex justify-center">
+            <StaticImage src="../images/testimonial-john.jpg" alt="John's ketamine testimonial" className="mx-auto rounded-full lg:w-3/4" />
+            </div>
+              <p className="text-center text-gray-600">“This is a very respectable and professional place the staff is is nice and welcoming, I keep coming back because they make me feel comfortable with myself.”</p>
+              <p className="text-center font-semibold">Ramiro C.</p>
+              <p className="text-center text-sm text-gray-500">Pacific Pain & Wellness Client</p>
+            </div>
+            {/* Testimonial 3 */}
+            <div className="space-y-4">
+            <div className="flex justify-center">
+            <StaticImage src="../images/testimonial-david.jpg" alt="John's ketamine testimonial" className="mx-auto rounded-full lg:w-3/4" />
+            </div>
+              <p className="text-center text-gray-600">“I loved my experience here. I came here with chronic pain & depression & received TMS & ketamine therapy. Changed my life, for the BETTER . I came for treatments Every day (xcept weekends) for many weeks. I looked forward to each visit as I continued to feel the Positive effects of the treatments I receeived. ”</p>
+              <p className="text-center font-semibold">Joshua B.</p>
+              <p className="text-center text-sm text-gray-500">Pacific Pain & Wellness Client</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      {/* Fifth Section */}
+
+
+      <section id="contact" className="bg-gradient-to-r from-blue-500 to-teal-500 py-20">
+        <div className="max-w-screen-xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
+          <div className="mb-8 md:mb-0 md:mr-8">
+            <h2 className="text-4xl font-bold text-white mb-4">Transform Your Tomorrow Today.</h2>
+            <p className="text-lg text-white mb-6"> Discover how personalized ketamine therapy can unlock a future of well-being and endless potential.</p>
+            
+            <div className="inline-block pt-4 mt-6 py-6 ">
+            <ButtonKetamine size="lg" variant="secondary">Take the Assessment</ButtonKetamine>
+            </div>
+
+          </div>
+          <StaticImage src="../images/change-your-life-today.png" alt="Pacific Pain & Wellness Group Logo" className="w-full md:w-1/2 h-auto" />
+        </div>
+      </section>
+
+
+    </Layout2>
+  );
+};
+
+export default Inside;
